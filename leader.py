af5f641cb10b1501ff03fc0971175efd0c4d6059
Muhammad Kamran <mkrnaqeebi@gmail.com>
2019-07-28 07:21:34 +0800

    fliping matrix with report

--- leader.py
+++ leader.py
@@ -1,46 +1,30 @@
-import bisect  
-import copy
 
-def findMajority(arr, size): 
+def findMajority(arr, size, start, end): 
     m = {} 
     for i in range(size): 
-        if arr[i] in m: 
-            m[arr[i]] += 1
+        i_value = arr[i]
+
+        if i>=start and i<end:
+            i_value +=1
+        if i_value in m: 
+            m[i_value] += 1
         else: 
-            m[arr[i]] = 1
-    count = 0
+            m[i_value] = 1
     for key in m: 
         if m[key] > size / 2: 
-            count = 1
             return key
-    if(count == 0): 
-        return None
-
-def find_leader_from_segment(K, A, N, index):
-    temp = copy.deepcopy(A)
-    for i in range(K):
-        temp[index+i] = A[index+i]+1
-    return findMajority(temp, N)
-
-def insert(list, I): 
-    bisect.insort(list, I)  
-    return list
+    return None
 
 def solution(K, M, A):
     # write your code in Python 3.6
     result = []
     N = len(A)
     for i in range(len(A)+1-K):
-        value_to_insert = find_leader_from_segment(K, A, N, i)
-        if value_to_insert:
-            if value_to_insert in result:
-                pass
-            else:
-                result = insert(result,value_to_insert)
-    return result
-    
+        temp = findMajority(A, N, i, i+K)
+        if temp:
+            result.append(temp)
+    return list(sorted(set(result)))
 
 
 print(solution(3, 5, [2, 1, 3, 1, 2, 2, 3]) )
-
 print(solution(4, 2, [1, 2, 2, 1, 2]))
